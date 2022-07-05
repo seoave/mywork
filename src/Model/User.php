65b2d60b199d1a5b42d924ba003feab413aa1f75
Seoave <seo.ave@gmail.com>
2022-07-05 19:13:13 +0300

    Add User

---
+++ src/Model/User.php
@@ -0,0 +1,27 @@
+<?php
+
+class User
+{
+    private string $login;
+    private string $email;
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): void
+    {
+        $this->login = $login;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+}
