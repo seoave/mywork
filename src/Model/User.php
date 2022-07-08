811e5551c20d72681f65686a6df37d712b6ef158
Seoave <seo.ave@gmail.com>
2022-07-08 23:44:00 +0300

    Add Models

--- src/Model/User.php
+++ src/Model/User.php
@@ -1,9 +1,67 @@
 <?php
 
+namespace App\Model;
+
 class User
 {
     private string $login;
     private string $email;
+    private string $name;
+    private string $role;
+    private string $country;
+    private string $city;
+    private string $phone;
+    private string $photo;
+
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): void
+    {
+        $this->country = $country;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): void
+    {
+        $this->city = $city;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
+    public function getPhoto(): string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): void
+    {
+        $this->photo = $photo;
+    }
+
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): void
+    {
+        $this->role = $role;
+    }
 
     public function getLogin(): string
     {
@@ -23,5 +81,15 @@ class User
     public function setEmail(string $email): void
     {
         $this->email = $email;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
     }
 }
