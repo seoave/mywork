7ac53f7e82e02654d883a7a3378b064e4720c96e
Seoave <seo.ave@gmail.com>
2022-07-16 16:31:38 +0300

    Add time measurement

--- data-structure/Stack/index.php
+++ data-structure/Stack/index.php
@@ -5,14 +5,35 @@ include_once __DIR__ . '/Stack.php';
 $node = new Node(1);
 $stack = new Stack();
 
-$stack
-    ->push('sss')
-    ->push('aaa')
-    ->push('ddd')
-    ->pop()
-    ->push(888)
-    ->pop()
-    ->pop()
-    ->pop();
+$n = 1000; // 10000, 1000000
 
-var_dump($stack->peek());
+// test stack push()
+
+$pushStartTime = microtime(true);
+
+for ($i = 0; $i < $n; $i++) {
+    $stack->push(random_int(1, 1000000));
+}
+
+echo 'Push for ' . $n . ' elements = ' . (microtime(true) - $pushStartTime) . "\n";
+
+// test stack pop()
+
+/*$popStartTime = microtime(true);
+
+for ($i = 0; $i < $n; $i++) {
+    $stack->pop();
+}
+
+echo 'Pop for ' . $n . ' elements = ' . (microtime(true) - $popStartTime) . "\n";*/
+
+// test stack peek()
+
+$peekStartTime = microtime(true);
+
+for ($i = 0; $i < $n; $i++) {
+    $stack->peek();
+    $stack->pop();
+}
+
+echo 'Peek for ' . $n . ' elements = ' . (microtime(true) - $peekStartTime) . "\n";
