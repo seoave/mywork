ffdfbd8574a9558d9311b4e9b3b0fe215bcb0675
Seoave <seo.ave@gmail.com>
2022-08-07 13:51:12 +0300

    Rename html directory to templates

---
+++ templates/parts/footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/images/mywork-logo-2.png" alt="" width="100" height="100">
+                </a>
+                <p>2022 Copyright</p>
+            </div>
+        </div>
+    </div>
+</footer>
