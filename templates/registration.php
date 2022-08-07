8875915c324348f4fe5ff0f15927e5b910bb906a
Seoave <seo.ave@gmail.com>
2022-08-07 14:09:14 +0300

    Add images

--- templates/registration.php
+++ templates/registration.php
@@ -1,3 +1,9 @@
 <?php require_once __DIR__ . '/parts/header.php'; ?>
-
+<main class="pt-5">
+    <div class="container">
+        <div class="row">
+            form here
+        </div>
+    </div>
+</main>
 <?php require_once __DIR__ . '/parts/footer.php'; ?>
