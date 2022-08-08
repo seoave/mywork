1c4782de8b8f47cee9481cdccce84903d2ec3110
Seoave <seo.ave@gmail.com>
2022-08-08 08:56:46 +0300

    Register html done

--- templates/parts/footer.php
+++ templates/parts/footer.php
@@ -2,10 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <a class="navbar-brand" href="#">
-                    <img src="../assets/images/mywork-logo-2.png" alt="" width="100" height="100">
-                </a>
-                <p>2022 Copyright</p>
+                <p class="text-center">2022 Copyright</p>
             </div>
         </div>
     </div>
--- templates/registration.php
+++ templates/registration.php
@@ -2,7 +2,25 @@
 <main class="pt-5">
     <div class="container">
         <div class="row">
-            form here
+            <h1 class="text-center">Registration</h1>
+            <form id="registration" action="">
+                <input type="text" class="form-control mb-3" placeholder="Enter your login name">
+                <input type="email" class="form-control mb-3" placeholder="Enter your email">
+                <input type="password" class="form-control mb-3" placeholder="Enter your password">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="select-role" id="select-candidate" checked value="candidate">
+                    <label class="form-check-label" for="select-candidate">
+                        Candidate
+                    </label>
+                </div>
+                <div class="form-check mb-3">
+                    <input class="form-check-input" type="radio" name="select-role" id="select-hr" value="hr">
+                    <label class="form-check-label" for="select-hr">
+                        Recruiter
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+            </form>
         </div>
     </div>
 </main>
