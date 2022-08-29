49a7a567f45e40237234bb89549e31ac427286f3
Seoave <seo.ave@gmail.com>
2022-08-30 01:53:44 +0300

    Remove garbage

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
 
-use App\Model\User;
-use App\Repository\JsonRepository;
-
 require_once __DIR__ . '/bootstrap.php';
-
-$DS = DIRECTORY_SEPARATOR;
-$repository = new JsonRepository(__DIR__ . $DS . 'db' . $DS . 'users.json');
-
-/*$robot = new User('Mr. Robot', 'robot@robo.cop');
-$robot->setPassword('111');
-$repository->create($robot);*/
 
 $routes = array_merge(
     include(__DIR__ . '/routes/api.php'),
